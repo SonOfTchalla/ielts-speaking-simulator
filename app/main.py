56c1d542c0a7ddf8c87a919cbe516a6c23c20c31
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2025-02-01 16:31:13 +0200

    Add app entry point file, import needed libraries

---
+++ app/main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from speech_to_text import transcribe_audio
+from conversational_ai import generate_response
+from text_to_speech import speak_text
+from scoring import evaluate_response
+from feedback import generate_feedback
+
