b2a1e1d78ce6516dcd957f64dde19806eda29f88
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2025-02-01 16:43:22 +0200

    Add audio transcription function

--- app/speech_to_text.py
+++ app/speech_to_text.py
@@ -1,1 +1,15 @@
 import speech_recognition as sr
+
+# Transcribe the audio, using the microphone, try to transcribe else return error messages.
+def transcribe_audio():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        st.write("Listening...")
+        audio = recognizer.listen(source)
+        try:
+            text = recognizer.recognize_google(audio)
+            return text
+        except sr.UnknownValueError:
+            return "Sorry, I could not understand the audio."
+        except sr.RequestError:
+            return "Sorry, the service is down."
